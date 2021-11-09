9d95e47d9007383506c99073245c252e57a116d3
chr0n1x <heilong24@gmail.com>
2021-11-09 13:45:59 -0500

    <leader>f: telescope w/ treesitter LSP

--- nvim/lua/key-bindings.lua
+++ nvim/lua/key-bindings.lua
@@ -5,30 +5,31 @@ vim.g.mapleader = ' '
 -- Note that some of these require plugins
 
 -- buffer navigation
-nmap('n', '<leader>s',       ':/<C-r><C-w>/<CR>',                       {noremap = true})
+nmap('n', '<leader>s',       ':/<C-r><C-w>/<CR>',                               {noremap = true})
 
 -- directory/tree navigation
-nmap('n', '<leader><tab>',   ':CHADopen<CR>',                           {noremap = true})
-nmap('n', '<leader>p',      ':Telescope find_files<CR>',                {noremap = true})
-nmap('n', '<leader>g',      ':Telescope live_grep<CR>',                 {noremap = true})
-nmap('n', '<leader>y',      ':Telescope neoclip a<CR>',                 {noremap = true})
+nmap('n', '<leader><tab>',   ':CHADopen<CR>',                                   {noremap = true})
+nmap('n', '<leader>f',      ':lua require"telescope.builtin".treesitter{}<CR>', {noremap = true})
+nmap('n', '<leader>p',      ':Telescope find_files<CR>',                        {noremap = true})
+nmap('n', '<leader>g',      ':Telescope live_grep<CR>',                         {noremap = true})
+nmap('n', '<leader>y',      ':Telescope neoclip a<CR>',                         {noremap = true})
 
 -- tab navigation
-nmap('n', '<leader>n',       ':tabnext<CR>',                            {noremap = true})
-nmap('n', '<leader>b',       ':tabprevious<CR>',                        {noremap = true})
-nmap('n', '<leader>o',       ':tabe<space>',                            {noremap = true})
+nmap('n', '<leader>n',       ':tabnext<CR>',                                    {noremap = true})
+nmap('n', '<leader>b',       ':tabprevious<CR>',                                {noremap = true})
+nmap('n', '<leader>o',       ':tabe<space>',                                    {noremap = true})
 
 -- editor visuals & "ergonomics"
-nmap('n', '<leader>G',       ':GitGutterLineHighlightsToggle<CR>',      {noremap = true})
-nmap('n', '<leader>z',       ':ZenMode<CR>',                            {noremap = true})
-nmap('n', '<leader>m',       ':set mouse=a<CR>',                        {noremap = true})
-nmap('n', '<leader>M',       ':set mouse=c<CR>',                        {noremap = true})
+nmap('n', '<leader>G',       ':GitGutterLineHighlightsToggle<CR>',              {noremap = true})
+nmap('n', '<leader>z',       ':ZenMode<CR>',                                    {noremap = true})
+nmap('n', '<leader>m',       ':set mouse=a<CR>',                                {noremap = true})
+nmap('n', '<leader>M',       ':set mouse=c<CR>',                                {noremap = true})
 
 -- misc 
-nmap('n', '<leader><space>', ':noh <bar> e<CR>',                        {noremap = true})
-nmap('n', '<leader>w',       ':w<CR>',                                  {noremap = true})
-nmap('n', '<leader>q',       ':q<CR>',                                  {noremap = true})
+nmap('n', '<leader><space>', ':noh <bar> e<CR>',                                {noremap = true})
+nmap('n', '<leader>w',       ':w<CR>',                                          {noremap = true})
+nmap('n', '<leader>q',       ':q<CR>',                                          {noremap = true})
 
 -- utility scripts
 -- pretty-format JSON in the current buffer/file
-nmap('n', '<leader>j',       ':%!python3 -m json.tool --sort-keys<CR>', {noremap = true})
+nmap('n', '<leader>j',       ':%!python3 -m json.tool --sort-keys<CR>',         {noremap = true})
