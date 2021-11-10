ec4e19c343dea764714239079224e1e18e9497ff
chr0n1x <heilong24@gmail.com>
2021-11-10 11:23:23 -0500

    init.lua: start telescope iff command === `nvim`

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,4 +5,7 @@ require('colors')
 require('base-settings')
 require('key-bindings')
 
-require('telescope.builtin').find_files()
+-- called with no other arguments (i.e.: command was just `nvim`)
+if #vim.v.argv == 1 then
+  require('telescope.builtin').find_files()
+end
