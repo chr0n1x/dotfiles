e17ad5dd80e4252efcecf3685de1bb2684bcb155
Kevin Ran <kran@adobe.com>
2023-01-05 16:51:07 -0500

    treesitter: specify languages

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'maintained',
+  ensure_installed = {'vim', 'lua', 'bash', 'yaml', 'json', 'hcl', 'make' },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
