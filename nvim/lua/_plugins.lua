9e4fd7ea142f61c3402001443d830ecea9a3fbe1
Kevin Ran <kran@adobe.com>
2024-02-29 10:22:50 -0500

    feat: LSP things

--- nvim/lua/_plugins.lua
+++ nvim/lua/_plugins.lua
@@ -73,6 +73,30 @@ return require('packer').startup(
         end
       }
 
+      -- copy-pasta from https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/packer.lua
+      use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v1.x',
+          requires = {
+              -- LSP Support
+              {'neovim/nvim-lspconfig'},
+              {'williamboman/mason.nvim'},
+              {'williamboman/mason-lspconfig.nvim'},
+
+              -- Autocompletion
+              {'hrsh7th/nvim-cmp'},
+              {'hrsh7th/cmp-buffer'},
+              {'hrsh7th/cmp-path'},
+              {'saadparwaiz1/cmp_luasnip'},
+              {'hrsh7th/cmp-nvim-lsp'},
+              {'hrsh7th/cmp-nvim-lua'},
+
+              -- Snippets
+              {'L3MON4D3/LuaSnip'},
+              {'rafamadriz/friendly-snippets'},
+          }
+      }
+
       -- git things
       use {
         'lewis6991/gitsigns.nvim',
