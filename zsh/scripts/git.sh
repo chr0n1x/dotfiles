f8c8cdcc1ab9df1f09402eee36116d797cc21676
Kevin Ran <kran@adobe.com>
2024-02-29 09:00:56 -0500

    feat: add even more git aliases

---
+++ zsh/scripts/git.sh
@@ -0,0 +1,41 @@
+#autoload
+#!/bin/bash
+
+# git aliases because my fingers are getting arthritis
+
+alias gp="git pull --rebase"
+alias gs="git status"
+
+unalias gr # AHHHHHHHHHHHHHHHHHHHH
+function gr() {
+  expectedRemoteName="upstream"
+
+  # if number of remotes is 1, assume I know what Im doing...? :/
+  numRemotes=$(git remote -v | awk '{print $1}' | uniq | wc -l | tr -d '[:blank:]')
+  if (( $numRemotes == 1 )); then
+    expectedRemoteName=$(git remote | tr -d '[:blank:]')
+  fi
+
+  remoteMainBranch=$(git remote show $expectedRemoteName | grep 'HEAD branch' | cut -d: -f2 | tr -d '[:blank:]')
+
+  if (( $numRemotes == 1 )); then
+    remoteUrl=$(git remote -v | grep "$expectedRemoteName" | grep fetch | awk '{print $2}')
+    echo "[WARN] Rebasing against '$remoteMainBranch' in $remoteUrl"
+  fi
+  git rebase "${expectedRemoteName}/${remoteMainBranch}" -i
+}
+
+unalias gf # AHHHHHHHHHHHHHHHHHHHH
+function gf() { git commit --fixup "$@" }
+
+# force push current branch, but only if we're pushing to our own fork
+function gpoh() {
+  pushRemote="$(git remote -v | grep origin | grep push | awk '{print $2}')"
+
+  if ! echo "$pushRemote" | grep $USER &> /dev/null; then
+    echo "[FATAL] 'origin' $pushRemote does NOT look like your fork!"
+    return 1
+  fi
+
+  git push origin +HEAD
+}
