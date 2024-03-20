ceae8f0ff4a68669a9b9322eb2237dbc4a907d41
Kevin Ran <kran@adobe.com>
2024-03-19 22:58:57 -0400

    chore: tweak base settings

--- nvim/lua/base-settings.lua
+++ nvim/lua/base-settings.lua
@@ -1,26 +1,27 @@
 local opts = vim.opt
 
 -- base vim config - bools
-opts.autoread      = true
-opts.autoindent    = false
-opts.cursorline    = true
-opts.expandtab     = true
-opts.gdefault      = true
-opts.hidden        = true
-opts.hlsearch      = true
-opts.ignorecase    = true
-opts.incsearch     = true
-opts.lazyredraw    = true
-opts.compatible    = false
-opts.number        = true
-opts.ruler         = true
-opts.showmatch     = true
-opts.showmode      = true
-opts.showcmd       = true
-opts.smartindent   = true
-opts.termguicolors = true
-opts.ttyfast       = true
-opts.wildmenu      = true
+opts.autoread       = true
+opts.autoindent     = false
+opts.cursorline     = true
+opts.expandtab      = true
+opts.gdefault       = true
+opts.hidden         = true
+opts.hlsearch       = true
+opts.ignorecase     = true
+opts.incsearch      = true
+opts.lazyredraw     = true
+opts.compatible     = false
+opts.number         = true
+opts.ruler          = true
+opts.relativenumber = true
+opts.showmatch      = true
+opts.showmode       = true
+opts.showcmd        = true
+opts.smartindent    = true
+opts.termguicolors  = true
+opts.ttyfast        = true
+opts.wildmenu       = true
 
 -- base vim config - non-bools
 vim.api.nvim_command([[filetype plugin indent on]])
@@ -29,8 +30,10 @@ opts.backspace      = 'indent,eol,start'
 opts.completeopt    = "menu,menuone,noselect"
 opts.encoding       = 'utf-8'
 opts.formatoptions  = 'qrn1'
+opts.foldexpr       = "v:lua.vim.treesitter.foldexpr()"
 opts.foldlevelstart = 99
-opts.foldmethod     = 'indent'
+opts.foldmethod     = 'syntax'
+opts.foldtext       = "v:lua.vim.treesitter.foldtext()"
 opts.guifont        = 'Consolas 20'
 opts.laststatus     = 2
 opts.regexpengine   = 1
