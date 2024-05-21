3d8d62ec075268769c5360f68ef25beb1e72dc6a
chr0n1x <heilong24@gmail.com>
2024-05-21 00:10:09 -0400

    fix: dun goofed myself

--- make/git.mk
+++ make/git.mk
@@ -1,6 +1,5 @@
 setup-git:
-	git config --global --replace-all core.pager "less -F -X"
 	git config --global rebase.autosquash true
 	git config --global color.ui true
 	git config --global core.editor vim
-	git config --global pager.branch false
+	git config --global core.pager cat
