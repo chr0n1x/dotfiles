322fa994e933218f27406ac0f60691a7e81cfcff
chr0n1x <heilong24@gmail.com>
2024-06-30 03:58:31 +0000

    fix: make git commit editor nvim

--- make/git.mk
+++ make/git.mk
@@ -1,6 +1,6 @@
 setup-git:
 	git config --global rebase.autosquash true
 	git config --global color.ui true
-	git config --global core.editor vim
+	git config --global core.editor nvim
 	git config --global core.pager cat
 	git config --global --replace-all core.pager "less -F -X"
